5289a7f2a576f5b2f29d3854280b1fe917e61c5c
Wesley  Alves Monteiro <wesley.monteiro@blueshift.com.br>
2023-01-29 18:21:52 -0300

    ChangeNotifierProvider added to app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,21 +2,25 @@ import 'package:contactsprovider/screens/contact_form/contact_form.dart';
 import 'package:contactsprovider/screens/home.dart';
 import 'package:contactsprovider/styles/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/contact_provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Contacts Provider",
-        theme: myCustomTheme,
-        initialRoute: '/',
-        routes: {
-          "/": (context) => const Home(),
-          '/form': (context) => const ContactForm(),
-        });
+    ;
+    return ChangeNotifierProvider(
+        create: (context) => ContactsProvider(),
+        child: MaterialApp(
+            title: "Contacts Provider",
+            theme: myCustomTheme,
+            initialRoute: '/',
+            routes: {
+              "/": (context) => const Home(),
+              '/form': (context) => const ContactForm(),
+            }));
   }
 }
