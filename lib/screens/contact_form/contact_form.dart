fce7a257ba535eb0bdaa8212157fed3e7cb1b60c
Wesley  Alves Monteiro <wesley.monteiro@blueshift.com.br>
2023-01-29 18:28:24 -0300

    added validation and controllers to form and a button to save new contacts

--- lib/screens/contact_form/contact_form.dart
+++ lib/screens/contact_form/contact_form.dart
@@ -1,4 +1,6 @@
+import 'package:contactsprovider/providers/contact_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ContactForm extends StatelessWidget {
   const ContactForm({super.key});
@@ -23,6 +25,11 @@ class _ContactFormInputs extends StatelessWidget {
   Widget build(BuildContext context) {
     final _contactFormKey = GlobalKey<FormState>();
 
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController phoneController = TextEditingController();
+    final TextEditingController typeController = TextEditingController();
+    final TextEditingController imageController = TextEditingController();
+    final TextEditingController bdController = TextEditingController();
     return Form(
       key: _contactFormKey,
       child: Column(
@@ -33,6 +40,7 @@ class _ContactFormInputs extends StatelessWidget {
             children: [
               // NAME
               TextFormField(
+                controller: nameController,
                 validator: (value) => value == "" ? "Type a Name" : null,
                 decoration: const InputDecoration(
                   hintText: "Name",
@@ -54,6 +62,7 @@ class _ContactFormInputs extends StatelessWidget {
               ),
               // PHONE NUMBER
               TextFormField(
+                controller: phoneController,
                 validator: (value) =>
                     value == "" ? "Type a Phone Number" : null,
                 decoration: const InputDecoration(
@@ -76,12 +85,36 @@ class _ContactFormInputs extends StatelessWidget {
               ),
               // TYPE OF NUMBER
               TextFormField(
+                controller: typeController,
                 validator: (value) =>
                     value == "" ? "Please speficy the type of phone" : null,
                 decoration: const InputDecoration(
                   hintText: "Type",
                   prefixIcon: Icon(
                     Icons.edit_note_sharp,
+                  ),
+                  filled: true,
+                  fillColor: Color.fromARGB(31, 151, 150, 150),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // BirthDay
+              TextFormField(
+                controller: bdController,
+                validator: (value) =>
+                    value == "" ? "Please speficy the birthday" : null,
+                decoration: const InputDecoration(
+                  hintText: "Type",
+                  prefixIcon: Icon(
+                    Icons.calendar_month,
                   ),
                   filled: true,
                   fillColor: Color.fromARGB(31, 151, 150, 150),
@@ -120,7 +153,18 @@ class _ContactFormInputs extends StatelessWidget {
           ElevatedButton(
               onPressed: () {
                 if (_contactFormKey.currentState!.validate()) {
-                  SnackBar(content: Text('teste'));
+                  final contactProvider =
+                      Provider.of<ContactsProvider>(context, listen: false);
+
+                  contactProvider.insert(
+                    name: nameController.text,
+                    type: typeController.text,
+                    birthday: bdController.text,
+                    image: imageController.text.isEmpty
+                        ? "https://ih1.redbubble.net/image.4598476168.2173/st,small,507x507-pad,600x600,f8f8f8.jpg"
+                        : imageController.text,
+                    phone: phoneController.text,
+                  );
                 }
               },
               child: Text("Save contact"))
