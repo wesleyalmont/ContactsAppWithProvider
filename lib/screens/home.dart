735cdee4b59d06da714b4f951615b97a6d5e9014
Wesley  Alves Monteiro <wesley.monteiro@blueshift.com.br>
2023-01-29 12:17:26 -0300

    ContactCard included at Home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
 
+import 'package:contactsprovider/mocks/contacts.dart';
+import 'package:contactsprovider/screens/components/contact_card.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,8 +10,19 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Text("Theme"),
+      appBar: AppBar(
+        title: const Text("Contacts"),
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return ContactCard(
+              contact: contacts[index],
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
