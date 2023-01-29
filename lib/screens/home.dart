3ff3ebc035a38c733b135973bcbbf278955307d4
Wesley  Alves Monteiro <wesley.monteiro@blueshift.com.br>
2023-01-29 14:10:26 -0300

    FAB navigation defined to go to formscreen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,7 +25,9 @@ class Home extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pushNamed(context, '/form');
+        },
         child: const Icon(Icons.add_outlined),
       ),
     );
